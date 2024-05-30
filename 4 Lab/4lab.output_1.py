5500ff541f80cd2afe5379cf23710852b14ad488
VladSad55 <154800959+VladSad55@users.noreply.github.com>
2024-05-30 13:50:48 +0300

    Update 4lab.output_1.py

--- 4 Lab/4lab.output_1.py
+++ 4 Lab/4lab.output_1.py
@@ -1,14 +1,14 @@
-class Animal:
-    def speak(self):
-        pass
+class Square:
+    def area_square(self, side):
+        return side ** 2
 
-    def move(self):
-        pass
-
-class Dog(Animal):
-    pass
+class Circle:
+    def area_circle(self, radius):
+        return 3.14 * radius ** 2
 
 # Приклад використання класів
-dog = Dog()
-dog.speak()
-dog.move()
+square = Square()
+print("Area of square:", square.area_square(5))
+
+circle = Circle()
+print("Area of circle:", circle.area_circle(3))
